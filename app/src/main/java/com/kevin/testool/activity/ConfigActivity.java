e55332fa2d668877e8bec3f573ca8c1c8175374b
likai.yang <likai.yang@jiduauto.com>
2023-01-18 17:07:35 +0800

    优化配置管理页面

--- app/src/main/java/com/kevin/testool/activity/ConfigActivity.java
+++ app/src/main/java/com/kevin/testool/activity/ConfigActivity.java
@@ -6,16 +6,17 @@ import android.annotation.SuppressLint;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
 import com.kevin.share.Common;
 import com.kevin.share.utils.ToastUtils;
-import com.kevin.share.utils.logUtil;
 import com.kevin.testool.MyApplication;
 import com.kevin.testool.R;
 
@@ -52,10 +53,10 @@ public class ConfigActivity extends BasicActivity {
             LinearLayout viewItem = new LinearLayout(this);
             LinearLayout.LayoutParams vp = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            vp.setMargins(15, 5, 15, 5);
+            vp.setMargins(15, 5, 15, 20);
             viewItem.setLayoutParams(vp);//设置布局参数
             viewItem.setOrientation(LinearLayout.VERTICAL);// 设置子View的Linearlayout// 为垂直方向布局
-            viewItem.setBackgroundColor(getResources().getColor(R.color.alice_blue));
+            viewItem.setBackgroundColor(getResources().getColor(R.color.white));
 
             TextView tv1 = new TextView(this);
             TextView tv2 = new TextView(this);
@@ -96,6 +97,55 @@ public class ConfigActivity extends BasicActivity {
         rootView.addView(mainView);
         this.addContentView(rootView,lp);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_config, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.toConfigDetail) {
+            AlertDialog.Builder popWindow = new AlertDialog.Builder(ConfigActivity.this);
+            View dialogView = LayoutInflater.from(ConfigActivity.this).inflate(R.layout.case_detail,null);
+            //设置对话框标题
+            popWindow.setTitle("配置文件");
+//                设置对话框消息
+            popWindow.setView(dialogView);
+//                String res = FileUtils.readJsonFile(CONFIG_FILE);
+            String res = Common.reloadCONFIG().toString();
+            final TextView case_detail = dialogView.findViewById(R.id.case_detail);
+            case_detail.setText(res.replace("}},","}},\n").replace("\",", "\",\n").replace("\n\n", "\n"));
+            case_detail.setTextIsSelectable(true);
+
+//                 添加选择按钮并注册监听
+            popWindow.setPositiveButton("编辑", (dialog, which) -> {
+                AlertDialog.Builder editWindow = new AlertDialog.Builder(ConfigActivity.this);
+                View editWindowView = LayoutInflater.from(MyApplication.getContext()).inflate(R.layout.case_edit,null);
+//                    editWindow.setTitle("编辑用例");
+                editWindow.setView(editWindowView);
+                final EditText et = editWindowView.findViewById(R.id.caseEdit);
+                et.setText(res.replace("}},","}},\n").replace("\",", "\",\n").replace("\n\n", "\n"));
+                editWindow.setPositiveButton("保存", (dialog1, which1) -> {
+                    Common.updateCONFIG(et.getText().toString());
+
+                });
+                editWindow.setNegativeButton("取消", null);
+                editWindow.show();
+            });
+            popWindow.setNegativeButton("关闭", null);
+            //对话框显示
+            popWindow.show();
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private Object exchangeType(String value, String theType) throws Exception {
